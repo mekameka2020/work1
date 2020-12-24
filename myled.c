1317c461cc65a0fbc93e87f44de57db4577d1df8
mekameka <s19C1133HA@s.chibakoudai.jp>
2020-12-24 10:06:16 +0000

    Change a Copyright

--- myled.c
+++ myled.c
@@ -1,7 +1,6 @@
-  
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-3.0-only
 /*
- * Copyright (C) 2011 STRATO.  All rights reserved.
+ * Copyright (C) 2020 Ryuichi Ueda.  All rights reserved.
  */
 
 
